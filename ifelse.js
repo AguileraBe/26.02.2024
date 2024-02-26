75de13c3c199d593fc7a309e2432ada725dce447
Be <159550318+AguileraBe@users.noreply.github.com>
2024-02-26 12:30:50 -0300

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,20 @@
+    // Declara uma variável chamada x e atribui o valor 2 a ela.
+var x = 2;
+
+    // Declara uma variável chamada y e atribui o valor 2 a ela.
+var y = 2;
+
+    // Inicia uma estrutura condicional if, que verifica se o valor de x é igual ao valor de y.
+if (x==y) {
+
+    // Se a condição dentro do if for verdadeira (ou seja, se x for igual a y), esta linha imprime a mensagem "Números conferem!" no console.
+    console.log("Números conferem!");
+
+    // Inicia o bloco de código a ser executado se a condição dentro do if for falsa.
+} else {
+
+    // Se a condição dentro do if for falsa (ou seja, se x não for igual a y), esta linha imprime a mensagem "Números divergem!" no console.
+    console.log("Números divergem!")
+
+    // Fecha o bloco de código do else.
+}
